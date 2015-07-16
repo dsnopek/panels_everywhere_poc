d3e37197d9f0ea96d81ef2f3b02304da4b91be1e
David Snopek <dsnopek@gmail.com>
2015-07-16 23:52:09 +0000

    Initial commit.

---
+++ src/EventSubscriber/PanelsEverywherePageDisplayVariantSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\panels_everywhere_poc\EventSubscriber\PanelsEverywherePageDisplayVariantSubscriber.
+ */
+
+namespace Drupal\panels_everywhere_poc\EventSubscriber;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Render\PageDisplayVariantSelectionEvent;
+use Drupal\Core\Render\RenderEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Selects the appropriate page display variant from 'site_template'.
+ */
+class PanelsEverywherePageDisplayVariantSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Selects the page display variant.
+   *
+   * @param \Drupal\Core\Render\PageDisplayVariantSelectionEvent $event
+   *   The event to process.
+   */
+  public function onSelectPageDisplayVariant(PageDisplayVariantSelectionEvent $event) {
+    // @todo: do the magic here!
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    $events[RenderEvents::SELECT_PAGE_DISPLAY_VARIANT][] = array('onSelectPageDisplayVariant');
+    return $events;
+  }
+
+}
